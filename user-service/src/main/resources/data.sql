557b020d9bd8a14aae0900112f479b60c0f29470
Artem Vlasenko <artem.vlasenko@gmail.com>
2019-03-28 14:33:33 +0200

    Added sql schema execution and test data insertion on application start-up

---
+++ user-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO artbook.users (first_name, last_name)
+VALUES ('John', 'Dou');
+INSERT INTO users (first_name, last_name)
+VALUES ('Art', 'Smith');
--- user-service/src/main/resources/schema.sql
+++ user-service/src/main/resources/schema.sql
@@ -1,11 +1,7 @@
 CREATE TABLE IF NOT EXISTS users
 (
-  user_id    INT NOT NULL AUTO_INCREMENT,
+  user_id    INT(20) NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(50),
-  last_name  VARCHAR(50)
+  last_name  VARCHAR(50),
+  PRIMARY KEY (user_id)
 );
-
-INSERT INTO users (first_name, last_name)
-VALUES ('John', 'Dou');
-INSERT INTO users (first_name, last_name)
-VALUES ('Art', 'Smith');
